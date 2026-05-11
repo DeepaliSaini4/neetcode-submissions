753e09737637a9d32c2d76b0c348de8ef1676080
Deepali Saini <deepalisa4444@gmail.com>
2026-05-12 02:25:46 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int l =0;
+        int count =0;
+        int n = people.size();
+        int r = n-1;
+        while(l<=r) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+                r--;
+                count++;
+            } else {
+                r--;
+                count++;
+            }
+        }
+        return count;
+    }
+};
