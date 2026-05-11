ce178e8454956805b6f8969ffcb1db1eb99f41e2
Deepali Saini <deepalisa4444@gmail.com>
2026-05-11 21:44:09 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;  // one minus size as 0 based
+        // O(n)
+        while (l <= r) {
+            while (l < r && !isalnum(s[l])) l++;  // skipping multiple invalid characters
+            while (l < r && !isalnum(s[r])) r--;  // skipping multiple invalid characters
+            if (l <= r) {
+                if (tolower(s[l]) != tolower(s[r])) return false;  // converting to lower to unify
+                l++;                                               // increment if a match
+                r--;                                               // decreement if a match
+            }
+        }
+        return true;  // met no false case
+    }
+};
