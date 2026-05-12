c1caf1859e338e236e267931cb59c4e79c92efad
Deepali Saini <deepalisa4444@gmail.com>
2026-05-12 20:32:52 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+        int head = -1;
+        int tail = 0;
+        int left = 0;
+        int maxf = 0;
+        int ans = 0;
+        vector<int>hash(26,0);
+        while(tail<n){
+            while(head+1<n && left<=k){
+                int idx = hash[s[head+1]-'A'];
+                int newmaxf = max(maxf,idx+1);
+                int newlen = (head+1) - tail + 1;
+                if(newlen-newmaxf>k){
+                    break;
+                }
+                head++;
+                hash[s[head]-'A']++;
+                maxf = max(maxf,hash[s[head]-'A']);
+                left = (head-tail+1) - maxf;
+            }
+            ans = max(head-tail+1,ans);
+            hash[s[tail]-'A']--;
+            tail++;
+        }
+        return ans;
+    }
+};
