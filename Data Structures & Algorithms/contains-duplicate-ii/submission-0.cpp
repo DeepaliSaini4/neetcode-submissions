c8cb81b3631855332a07ba33dfa1fe61c631ee18
Deepali Saini <deepalisa4444@gmail.com>
2026-05-12 15:15:19 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        int n = nums.size();
+        int head = -1;
+        int tail = 0;
+        while (tail < n) {
+            while (head + 1 < n && head - tail + 1 <= k) {
+                if (s.find(nums[head + 1]) != s.end()) return true;
+                head++;
+                s.insert(nums[head]);
+            }
+            if (tail > head) {
+                tail++;
+                head = tail - 1;
+            } else {
+                s.erase(nums[tail]);
+                tail++;
+            }
+        }
+        return false;
+    }
+};
